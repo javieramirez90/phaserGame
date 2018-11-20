a8dbc1834efe650081bb5179ca0f4c7518e0b0c1
javieramirez90 <javier.ramirez@aiesec.net>
2018-11-20 16:51:38 -0600

    Intro finished

--- js/game.js
+++ js/game.js
@@ -6,27 +6,62 @@ gameScene.preload = function() {
   //Loading background
   this.load.image('background', '../assets/background.png');
   this.load.image('player', '../assets/player.png');
+  this.load.image('enemy', '../assets/dragon.png');
 };
 
 //called after the preloads ends.
 gameScene.create = function() {
   //create background sprite
-  let bg = this.add.sprite(0, 0, 'background');
+  this.bg = this.add.sprite(0, 0, 'background');
   //changing the origin to the top left corner
   //bg.setOrigin(0,0);
-  let gameW = this.sys.game.config.width;
-  let gameH = this.sys.game.config.height;
-  bg.setPosition(gameW/2, gameH/2);
+  this.gameW = this.sys.game.config.width;
+  this.gameH = this.sys.game.config.height;
+  this.bg.setPosition(this.gameW/2, this.gameH/2);
 
   //creating player sprite
-  let player = this.add.sprite(50, 180, 'player');
+  this.player = this.add.sprite(50, 180, 'player');
   // player.depth = 1;
   //the dept property allow us to put our elements in order they appear in our canvas.
   //in order to not worry for the place we put them. The higher the number of the depth
   //the more on top the element is.
   
   /*SCALING THE SPRITE*/
-  player.setScale(2, 2);
+  this.player.setScale(.5, .5);
+
+  //create an enemy
+  this.enemy1 =  this.add.sprite(250, 180, 'enemy');
+  
+
+  this.enemy1.scaleX = 2;
+  this.enemy1.scaleY = 2;
+
+  // enemy1.angle = 45;
+  // enemy1.setAngle(45); this give us the same result as the one before
+  // enemy1.setAngle(-45); this give us the same result as the one before but to the contrary clockwise 
+  // enemy1.rotation = -(Math.PI / 4);
+  //create a new enemy
+  this.enemy1.setRotation = -(Math.PI / 4);
+
+  this.enemy2 =  this.add.sprite(450, 180, 'enemy');
+  this.enemy2.displayWidth = 100;
+
+  //flip sprite
+  this.enemy1.flipX = true;
+
+}
+
+gameScene.update = function(){
+  // this.enemy1.x += 1;
+  this.enemy1.angle += 1;
+
+  //check if we checked the scale of two
+
+  if(this.player.scaleX < 2){
+    //make the player grow
+    this.player.scaleX += .01;
+    this.player.scaleY += .01;
+  }
 }
 
 //Set the congfiguration of the game
